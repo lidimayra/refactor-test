ec3cc310bac2b2a174f5e1f1e85296baab614ebc
Lidiane Taquehara <lidi.mayra@gmail.com>
2016-11-05 18:26:57 -0200

    Add models request to Webmotors service

--- app/services/webmotors.rb
+++ app/services/webmotors.rb
@@ -6,7 +6,18 @@ class Webmotors
 
     # Make request for Webmotors site
     response = Net::HTTP.post_form(uri, {})
-    json = JSON.parse response.body
+    JSON.parse response.body
+  end
+
+  def self.models(make_id)
+    #search the models
+    uri = URI("http://www.webmotors.com.br/carro/modelos")
+
+    # Make request for Webmotors site
+    make = Make.find_by(webmotors_id: make_id)
+
+    response = Net::HTTP.post_form(uri, { marca: make_id })
+    JSON.parse response.body
   end
 
 end
--- spec/services/webmotors_spec.rb
+++ spec/services/webmotors_spec.rb
@@ -17,4 +17,26 @@ RSpec.describe 'Webmotors', type: :services do
       it { is_expected.to eq keys }
     end
   end
+
+  describe 'models' do
+    let(:make) { create :make }
+    subject(:models) { Webmotors.models(make.id) }
+
+    it { is_expected.to be_an_instance_of Array }
+
+    context 'when make has no models' do
+      it { is_expected.to be_empty }
+    end
+
+    context 'when make has models' do
+      let(:make) { create :make, id: 2 }
+      subject { models.sample.keys }
+
+      let(:keys) do
+        ['Id', 'Nome', 'Count', 'NomeAmigavel']
+      end
+
+      it { is_expected.to eq keys }
+    end
+  end
 end
