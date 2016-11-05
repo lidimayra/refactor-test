a7fe03fd88b5dc9bd00ff3c3c0ce9c8fe88fdf03
Lidiane Taquehara <lidi.mayra@gmail.com>
2016-11-05 17:35:49 -0200

    Transfer Make persisting responsibility to model

--- app/models/make.rb
+++ app/models/make.rb
@@ -1,4 +1,13 @@
 class Make < ActiveRecord::Base
 
   validates :webmotors_id, presence: true, uniqueness: true
+  validates :name, uniqueness: true
+
+  def self.persist_if_absent
+    Webmotors.makes.each do |make_params|
+      Make.find_or_create_by(name: make_params["Nome"]) do |make|
+        make.webmotors_id = make_params["Id"]
+      end
+    end
+  end
 end
--- spec/models/make_spec.rb
+++ spec/models/make_spec.rb
@@ -2,9 +2,36 @@ require 'rails_helper'
 
 RSpec.describe Make, type: :model do
 
-  subject { build :make }
-  it { is_expected.to be_valid }
+  subject(:make) { build :make }
 
-  it { is_expected.to validate_presence_of :webmotors_id }
-  it { is_expected.to validate_uniqueness_of :webmotors_id }
+  context 'factory' do
+    it { is_expected.to be_valid }
+  end
+
+  context 'validations' do
+    it { is_expected.to validate_presence_of :webmotors_id }
+    it { is_expected.to validate_uniqueness_of :webmotors_id }
+    it { is_expected.to validate_uniqueness_of :name }
+  end
+
+  context 'when persisting makes' do
+    subject(:persist_if_absent) { Make.persist_if_absent }
+    before { create :make, name: 'FIAT' }
+
+    context 'when make is absent' do
+      it 'persists make' do
+        expect do
+          persist_if_absent
+        end.to change(Make.where(name: 'HONDA'), :count)
+      end
+    end
+
+    context 'when make is present' do
+      it "doesn't persist make" do
+        expect do
+          persist_if_absent
+        end.not_to change(Make.where(name: 'FIAT'), :count)
+      end
+    end
+  end
 end
