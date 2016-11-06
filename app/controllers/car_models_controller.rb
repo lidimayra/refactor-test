97a22681908a608d55437520fb33c4ce394bd564
Lidiane Taquehara <lidi.mayra@gmail.com>
2016-11-05 22:21:51 -0200

    Applies Decent Exposure to CarModelsController
    Remove global variables from view

--- app/controllers/car_models_controller.rb
+++ app/controllers/car_models_controller.rb
@@ -1,5 +1,8 @@
 class CarModelsController < ApplicationController
 
+  expose :make, -> { Make.find_by(webmotors_id: params[:webmotors_make_id]) }
+  expose :car_models, from: :make
+
   def index
     make = Make.find_by(webmotors_id: params[:webmotors_make_id])
     CarModel.persist_if_absent(make.id)
