9ef2b8dff61de470a427642160df6c4910032e04
Lidiane Taquehara <lidi.mayra@gmail.com>
2016-11-06 01:43:36 -0200

    Render not found when trying to access models of inexistent Make

--- spec/controllers/car_models_controller_spec.rb
+++ spec/controllers/car_models_controller_spec.rb
@@ -6,13 +6,21 @@ RSpec.describe CarModelsController, type: :controller do
   before { create :car_model, make: make }
 
   describe '#index' do
-    subject(:get_index) { get :index, webmotors_make_id: make.id }
 
-    render_views
-    it { is_expected.to render_template :index }
+    context 'when make exists' do
+      subject(:get_index) { get :index, webmotors_make_id: make.id }
 
-    it 'calls method to persist new makes' do
-      expect { get_index }.to change(CarModel, :count)
+      render_views
+      it { is_expected.to render_template :index }
+
+      it 'calls method to persist new makes' do
+        expect { get_index }.to change(CarModel, :count)
+      end
+    end
+
+    context "when make doesn't exist" do
+      subject { get :index }
+      it { is_expected.to be_not_found }
     end
   end
 
